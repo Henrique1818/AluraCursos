d14d8af0f09b97b18173c0ac84d9965b7a271992
Henrique1818 <luiz.henrique01827@gmail.com>
2020-11-29 19:42:51 -0300

    operadores aritimeticas em JS

---
+++ CursoJavascript/operadoresAritimeticas.js
@@ -0,0 +1,10 @@
+/* Operações Aritiméticas */
+
+console.log(2 + 2); // 4
+console.log(10 + 8 * 2); // 26 retornando valor errado
+console.log((10 + 8) * 2); // 36 retornando valor correto
+
+console.log("ano" + 2020) // ano2020 concatenação
+console.log("2" + 2) // 22
+
+console.log("10" / "2"); // 5, pois o javascript entende que dividir so faz sentindo ser for number, entao ele força a conversao dele;
