99a00e8dcef2963f48ebdae81c20356b6ea359f8
Henrique1818 <luiz.henrique01827@gmail.com>
2020-11-29 19:15:26 -0300

    boas praticas, trabalhando com variaveis

---
+++ CursoJavascript/trabalhandoComVariaveis.js
@@ -0,0 +1,15 @@
+/* Trabalhando com variaveis, boas praticas */
+// JS é case sensitive
+
+console.log('Boas praticas'); // Boas praticas
+//Console.log('Boas praticas') Error Console is not defined, não existe Console com C maiusculo.
+
+idade1 = 20; // 20, estou declarando uma variavel global forma errada de declarar uma variavel
+
+const idade = 19 // 19, forma correta de ser declarar uma variavel em JS.
+
+// Idade = 21; 21, mesmo que tenha o mesmo nome, porem o I em maiusculo, o javascript entende que é
+// uma nova variavel diferente pois o JS é case sensitive
+
+console.log(idade1);
+console.log(idade);
