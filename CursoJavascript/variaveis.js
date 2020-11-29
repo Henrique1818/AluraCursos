78b58cb4944005330ad287c7b0312512374ee2e1
Henrique1818 <luiz.henrique01827@gmail.com>
2020-11-29 18:36:09 -0300

    Variavel e Aritmética

---
+++ CursoJavascript/variaveis.js
@@ -0,0 +1,18 @@
+/* Guardado valores dentro de uma váriavel, onde são armazenadas na memoria do computador
+    e podem ser acessadas no mesmo codigo.
+*/
+
+const idade = 21;
+
+console.log(idade);
+
+/* Aritmética */
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade*2);
+console.log(idade/2);
+console.log(idade%2);
+
+
+let idadeSomada = idade + 3;
+console.log(idadeSomada);
