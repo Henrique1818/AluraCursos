599d5b0cdbf17a70a18a402ab5aad9def0ff0bed
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-01 07:08:41 -0300

    atribuicao e imutabilidade no Javascript

--- CursoJavascript/atribuicaoImutaveis.js
+++ CursoJavascript/atribuicaoImutaveis.js
@@ -1,14 +1,20 @@
 // Atribuição e Imutabilidade
 
-const primeiroNome = "Luiz"
-const sobrenome = "henrique"
+const primeiroNome = "Luiz";
+const sobrenome = "henrique";
 
-console.log(primeiroNome, sobrenome) // Luiz Henrique
-console.log(primeiroNome + " " + sobrenome) // Luiz Henrique
-console.log(`meu nome é ${primeiroNome} ${sobrenome}`)
+console.log(primeiroNome, sobrenome); // Luiz Henrique
+console.log(primeiroNome + " " + sobrenome); // Luiz Henrique
+console.log(`meu nome é ${primeiroNome} ${sobrenome}`);
 
 // primeiroNome = "Pedro"
 // console.log(primeiroNome) // Error assignment to constant variable
 
-const nomeCompleto = primeiroNome + sobrenome
-console.log(nomeCompleto)
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade;
+idade = 21;
+
+idade = idade + 1;
+console.log(idade); // 22
