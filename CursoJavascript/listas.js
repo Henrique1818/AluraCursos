cb06f86893724ffe84635c2b2eff1d184b4d1faa
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-01 07:30:10 -0300

    trabalhando com lista usando array e removendo itens e selecionado um unico item

--- CursoJavascript/listas.js
+++ CursoJavascript/listas.js
@@ -10,3 +10,7 @@ listaDeDestinos.push(`Ceará`) // Adicionar um iten a lista
 
 console.log('Destinos possíveis');
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1); // splice(posição, quantidade) a ser apagada
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[2], listaDeDestinos[1]);
