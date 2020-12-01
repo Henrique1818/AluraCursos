5d57783e07b255109cca6e1345d4a18d468e5d82
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-01 07:22:16 -0300

    trabalhando com lista usando array e add item ao array com push

---
+++ CursoJavascript/listas.js
@@ -0,0 +1,12 @@
+// Trabalhando com listas
+
+const listaDeDestinos = new Array(
+    `São Paulo`,
+    `Rio de Janeiro`,
+    `Salvador`
+);
+
+listaDeDestinos.push(`Ceará`) // Adicionar um iten a lista
+
+console.log('Destinos possíveis');
+console.log(listaDeDestinos);
