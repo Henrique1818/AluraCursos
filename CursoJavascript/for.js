385767d3c0bda3f11843e664bf43b99000bf4302
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-11 21:33:24 -0300

    loop usando for... javascript

---
+++ CursoJavascript/for.js
@@ -0,0 +1,50 @@
+// Trabalhando com listas
+
+const listaDeDestinos = new Array(
+    `São Paulo`,
+    `Rio de Janeiro`,
+    `Salvador`
+);
+
+const idadeComprador = 21;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+let destino = 'Ceára';
+
+console.log('Destinos possiveis');
+console.log(listaDeDestinos);
+
+const podeComprar = (idadeComprador >= 18 || estaAcompanhada == true);
+
+let  contador = 0;
+let destionoExiste = false;
+while(contador < 3) {
+    if(listaDeDestinos[contador] == destino){
+        destionoExiste = true;
+        break; // para de execultar o loop
+    } else {
+        destionoExiste = false;
+    };
+
+    contador += 1;
+};
+
+console.log('Destino existe: ', destionoExiste);
+
+console.log('Destino existe: ', destionoExiste);
+
+if(podeComprar && destionoExiste) {
+    console.log('Boa Viagem!');
+} else {
+    console.log('Desculpe tivemos um erro!');
+};
+
+for(let cont = 0; cont < 3; cont++) {
+    if(listaDeDestinos[cont] == destino){
+        destionoExiste = true;
+    } else {
+        destionoExiste = false;
+    };
+
+    contador += 1;
+};
