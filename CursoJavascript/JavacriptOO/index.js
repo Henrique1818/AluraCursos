f0507753a02303101d2ec5721203d9c940f84022
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-13 20:19:44 -0300

    criando método/função de deposito e definido um atributo privado

--- CursoJavascript/JavacriptOO/index.js
+++ CursoJavascript/JavacriptOO/index.js
@@ -1,19 +1,31 @@
 class Cliente{
-    nome;
+    nome; // criando propriedades == criando atributos
     cpf;
 };
 
+// criando uma classe == criando um modelo
 class ContaCorrente{
     agencia;
-    saldo;
 
+    // passando nossa propriedade ou atributo
+    // para ser privado usando # ou _ porem _ não 100% privado ainda pode ser manipulada;
+    #saldo = 0;
+
+    // criando um método/função
     sacar(valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
+        if(this.#saldo >= valor) {
+            this.#saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this.#saldo += valor;
         }
     }
 };
 
+// instanciando nosso modelo para uma variavel == criando um Objeto;
 const cliente1 = new Cliente();
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 22233355509;
@@ -25,15 +37,8 @@ cliente2.cpf = 88833355509;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.saldo = 0;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-
+contaCorrenteRicardo.depositar(200);
 contaCorrenteRicardo.sacar(50);
 
-
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
