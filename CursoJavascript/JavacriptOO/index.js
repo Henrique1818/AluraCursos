c100c0bd830f986c7fe186aecd99402cac42d1f1
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-13 21:53:41 -0300

    composição de classes

--- CursoJavascript/JavacriptOO/index.js
+++ CursoJavascript/JavacriptOO/index.js
@@ -13,12 +13,23 @@ cliente2.cpf = 88833355509;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.agencia = 1002;
+conta2.cliente = cliente2;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+contaCorrenteRicardo.transferir(250, conta2);
 
+console.log(conta2);
 console.log(contaCorrenteRicardo);
+
+
+
+// contaCorrenteRicardo.depositar(100);
+// contaCorrenteRicardo.depositar(100);
+// contaCorrenteRicardo.depositar(100);
+
+// const valorSacado = contaCorrenteRicardo.sacar(50);
+// console.log(valorSacado);
