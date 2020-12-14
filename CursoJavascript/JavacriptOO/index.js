e5d7bee8b71356704f0123d9607467d76b4fa07b
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-14 07:03:30 -0300

    deixando o codigo mais enxuto

--- CursoJavascript/JavacriptOO/index.js
+++ CursoJavascript/JavacriptOO/index.js
@@ -2,23 +2,14 @@ import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 
 // instanciando nosso modelo para uma variavel == criando um Objeto;
-const cliente1 = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = 22233355509;
+const cliente1 = new Cliente('Ricardo', 22233355509);
+
+const cliente2 = new Cliente('Alice', 88833355509);
 
 
-const cliente2 = new Cliente();
-cliente2.nome = 'Alice';
-cliente2.cpf = 88833355509;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002;
+const conta2 = new ContaCorrente(1002, cliente2);
 
 
 let valor = 200;
