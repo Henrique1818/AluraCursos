8d4b0086482b664914e34696e1b4b593eb9d4281
Henrique1818 <luiz.henrique01827@gmail.com>
2020-12-14 06:45:51 -0300

    usando getters e setters

--- CursoJavascript/JavacriptOO/index.js
+++ CursoJavascript/JavacriptOO/index.js
@@ -7,9 +7,9 @@ cliente1.nome = 'Ricardo';
 cliente1.cpf = 22233355509;
 
 
-// const cliente2 = new Cliente();
-// cliente2.nome = 'Alice';
-// cliente2.cpf = 88833355509;
+const cliente2 = new Cliente();
+cliente2.nome = 'Alice';
+cliente2.cpf = 88833355509;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -17,10 +17,9 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = 'Alice';
-conta2.cliente.cpf = 88833355509
+conta2.cliente = cliente2;
 conta2.agencia = 1002;
+
 
 let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
@@ -28,6 +27,8 @@ contaCorrenteRicardo.transferir(valor, conta2);
 console.log(conta2);
 
 
+
+// assessores não são métodos, assessores são publicos (get e set);
 // null = alguem colocou diretamente esse valor vazio;
 // undefined = alguem esqueceu de defini algum valor a variavel;
 
