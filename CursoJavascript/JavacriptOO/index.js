b274316bae40990b3a564ac3709481d873b150d7
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-02 22:05:27 -0300

    Adicando uma conta poupança

--- CursoJavascript/JavacriptOO/index.js
+++ CursoJavascript/JavacriptOO/index.js
@@ -1,21 +1,18 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
+import { ContaPoupanca } from './ContaPoupanca.js';
 
 // instanciando nosso modelo para uma variavel == criando um Objeto;
 const cliente1 = new Cliente('Ricardo', 22233355509);
 
-const cliente2 = new Cliente('Alice', 88833355509);
-
-
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
-const conta2 = new ContaCorrente(1002, cliente2);
+contaCorrenteRicardo.sacar(100);
 
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
-
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
 
 
 
