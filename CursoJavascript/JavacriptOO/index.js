30919df134a08fe987dec5fb1e6bdebab5555e8a
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-03 17:16:42 -0300

    Refatorando os objetos

--- CursoJavascript/JavacriptOO/index.js
+++ CursoJavascript/JavacriptOO/index.js
@@ -1,10 +1,11 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 import { ContaPoupanca } from './ContaPoupanca.js';
+import { Conta } from './Conta.js';
 
 const cliente1 = new Cliente('Ricardo', 22233355509);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
@@ -12,6 +13,25 @@ const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
