d12712c9d3987749e7b315aa1c7bc561519e2fd6
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-03 17:15:55 -0300

    Criando herança utilizando o extends

--- CursoJavascript/JavacriptOO/ContaCorrente.js
+++ CursoJavascript/JavacriptOO/ContaCorrente.js
@@ -1,52 +1,11 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
 // criando uma classe == criando um modelo
-export class ContaCorrente{
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-
-    set cliente(novoValor) {
-        if(novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente() {
-        return this._cliente;
-    }
-
-    // passando nossa propriedade ou atributo
-    // para ser privado usando # ou _ usando somente dentro da classe criada;
-
-    get saldo() {
-        return this._saldo;
-    }
-
-    constructor(agencia, cliente) {
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
-    }
-
-    // criando um método/função
-    sacar(valor) {
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if(valor <= 0) return;
-
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
     }
 };
