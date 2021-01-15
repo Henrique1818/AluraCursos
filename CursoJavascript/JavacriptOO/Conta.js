cdf8e1a6c7c8010613f67653f5dd8628465c24ad
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-14 21:02:45 -0300

    não instanciando o tipo conta

--- CursoJavascript/JavacriptOO/Conta.js
+++ CursoJavascript/JavacriptOO/Conta.js
@@ -3,6 +3,11 @@ export class Conta{
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+
+        console.log(this.constructor);
+        if(this.constructor == Conta) {
+            console.log('Você não deveria instanciar um objeto do tipo conta');
+        }
     }
 
     set cliente(novoValor) {
